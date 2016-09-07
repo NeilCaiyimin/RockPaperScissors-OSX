43755d1dc3d1c440b37d93397cfca9d169265d01
Neil <bravc29229@gmail.com>
2016-09-07 23:37:30 +0800

    add a UI

---
+++ RockPaperScissors/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RockPaperScissors
+//
+//  Created by Neil Cai on 9/7/16.
+//  Copyright © 2016 Neil,Cai. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RockPaperScissors/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RockPaperScissors
+//
+//  Created by Neil Cai on 9/7/16.
+//  Copyright © 2016 Neil,Cai. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
